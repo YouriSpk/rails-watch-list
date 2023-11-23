01ea6e4dc8bb2fc02eae1a5470affded83bc3628
Youri Soposki <youri.soposki@gmail.com>
2023-11-23 15:51:51 +0100

    progressing

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   # route for LIST
-  get '/lists', to: 'lists#index', as: :lists
-  get '/lists/:id', to: 'lists#show', as: :list
-  get '/lists/new', to: 'lists#new', as: :new_list
-  post '/lists', to: 'lists#create'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
