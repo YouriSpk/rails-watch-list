85a245daf12efee5a71e841955df4ce56d37c2c6
Youri Soposki <youri.soposki@gmail.com>
2023-11-23 15:09:45 +0100

    route list and generation controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # route for LIST
+  get '/lists', to: 'lists#index', as: :lists
+  get '/lists/:id', to: 'lists#show', as: :list
+  get '/lists/new', to: 'lists#new', as: :new_list
+  post '/lists', to: 'lists#create'
 end
